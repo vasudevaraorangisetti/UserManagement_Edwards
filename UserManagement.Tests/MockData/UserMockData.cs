6825841b78f7fec2ba21aa76d95a69b073763dbd
Vasu <vasudevaraonaidu@live.com>
2022-07-30 23:20:30 +0100

    added test files

---
+++ UserManagement.Tests/MockData/UserMockData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Shared;
+
+namespace UserManagement.Tests
+{
+    public class UserMockData
+    {
+        internal static User GetUserDataForInsert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UserManagement.Tests/UnitTests/UserTest.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Shared;
+
+namespace UserManagement.Tests
+{
+    [TestFixture]
+    public class UserTest
+    {
+        HttpClient client = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(Shared.Constants.BaseAddress);
+            client.DefaultRequestHeaders.Accept.Clear();
+        }
+
+        [Test]
+        public async Task Retrun_Empty_UserDetailsSet()
+        {
+            //Data should be returned as NULL when there is no file
+            var response = client.GetAsync("api/User").Result;
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<User>>(jsonString);
+            Assert.AreEqual(0,result.Count);
+
+        }
+
+        [Test]
+        public async Task Insert_User_Data_True()
+        {
+            var user = UserMockData.GetUserDataForInsert();
+           
+            var response = await client.PostAsJsonAsync("api/User", user);
+            
+        }
+    }
+}
